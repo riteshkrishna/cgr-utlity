582ba82daf172501bd20d29589d3ad2c8be3b16b
riteshkrishna <riteshk@liv.ac.uk>
2013-07-01 14:26:11 +0100

    Batch execution of Bowtie2

--- src/org/liverpool/bootstrap/ExecuteBowtie2.java
+++ src/org/liverpool/bootstrap/ExecuteBowtie2.java
@@ -3,6 +3,8 @@ package org.liverpool.bootstrap;
 import java.io.*;
 import java.util.ArrayList;
 
+import org.liverpool.utils.GetCommandLineParameters;
+
 public class ExecuteBowtie2 {
 
 	/**
@@ -10,6 +12,24 @@ public class ExecuteBowtie2 {
 	 */
 	public static void main(String[] args) {
 		try{
+			
+			if(args.length < 16){
+				String cmd = "-bowtie_home bt_home -bowtie_db_path bowtie_db -fastq_path fastq -sam_path sam -screenout_path screenout" +
+						" -bowtie_script_file bowtie_script -bowtie_batch_run_output bowtie_batch_run -seed_step seed_step"; 
+	            System.out.println(cmd);
+			}
+	        
+			String bowtie_home = GetCommandLineParameters.getCmdParameter(args, "bowtie_home", true);
+			String bowtie_db_path = GetCommandLineParameters.getCmdParameter(args, "bowtie_db_path", true);
+			String fastq_path = GetCommandLineParameters.getCmdParameter(args, "fastq_path", true);
+			String sam_path = GetCommandLineParameters.getCmdParameter(args, "sam_path", true);
+			String screenout_path = GetCommandLineParameters.getCmdParameter(args, "screenout_path", true);
+			
+			String bowtie_script_file = GetCommandLineParameters.getCmdParameter(args, "bowtie_script_file", true);
+			String bowtie_batch_run_output = GetCommandLineParameters.getCmdParameter(args, "bowtie_batch_run_output", true);
+			int seed_step = Integer.parseInt(GetCommandLineParameters.getCmdParameter(args, "seed_step", true));
+			
+			/* 
 			String bowtie_home = "/Users/ritesh/Software/bowtie2-2.1.0-binary";
 			String bowtie_db_path = "/Users/ritesh/Ritesh_CGR_Work/bowtie-database/Ensembl-Human-gene-cdna";
 			String fastq_path = "/Users/ritesh/Ritesh_CGR_Work/Ortholog-data/Multi-Species-Approach/FromEnsemble/April/SpeciesWise-All-Human-Orth-Gene-HomologScript/FASTQ-Sequences/Fastq-Cfam.txt";
@@ -18,8 +38,9 @@ public class ExecuteBowtie2 {
 			
 			String bowtie_script_file = "/Users/ritesh/tmp/BT2_script"; 
 			String bowtie_batch_run_output = "/Users/ritesh/tmp/batch_run_output.txt";
-			
-			int seed_step = 2; 
+			int seed_step = 2;
+			*/
+			 
 			
 			BufferedWriter out_batch = new BufferedWriter(new FileWriter(new File(bowtie_batch_run_output)));
 			
